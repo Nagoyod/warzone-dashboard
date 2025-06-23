25bef4b8097da75cfff3f6e73864b09ba01c4767
Nagoyod <armandoyogan77@gmail.com>
2025-06-23 14:20:00 +0800

    Update navigation.ts

--- src/app/theme/layouts/admin-layout/navigation/navigation.ts
+++ src/app/theme/layouts/admin-layout/navigation/navigation.ts
@@ -99,21 +99,5 @@ export const NavigationItems: NavigationItem[] = [
     ]
   },
 
-  {
-    id: 'other',
-    title: 'Other',
-    type: 'group',
-    icon: 'icon-navigation',
-    children: [
-      {
-        id: 'sample-page',
-        title: 'Sample Page',
-        type: 'item',
-        url: '/sample-page',
-        classes: 'nav-item',
-        icon: 'chrome'
-      }
-      
-    ]
-  }
+ 
 ];
